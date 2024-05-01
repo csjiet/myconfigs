927d556a023ccda67045222a0f90afaec08f5e51
csjiet <cs.jiet@gmail.com>
2024-04-30 22:32:29 -0500

    changed to lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -199,7 +199,63 @@ vim.g.NERDTreeShowHidden = 1
 -- }
 
 local lualine = require('lualine')
-lualine.setup()
+local lualine = require('lualine')                                                                                                                                           
+lualine.setup {                                                                                                                                                              options = {
+	icons_enabled = true,																																																																											theme = 'auto', 
+},                                                                                                                                                                         sections = {
+		lualine_a = {
+			{'filename',                                                                                                                                                            file_status = true,      -- Displays file status (readonly status, modified status)
+				newfile_status = false,  -- Display new file status (new file means no write after created)
+				path = 1,                -- 0: Just the filename
+				-- 1: Relative path
+				-- 2: Absolute path
+				-- 3: Absolute path, with tilde as the home directory
+				-- 4: Filename and parent dir, with tilde as the home directory
+
+				shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+				-- for other components. (terrible name, any suggestions?)
+				symbols = {
+					modified = '[+]',      -- Text to show when the file is modified.
+					readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+					unnamed = '[No Name]', -- Text to show for unnamed buffers.
+					newfile = '[New]',     -- Text to show for newly created file before first write
+				}
+			}
+		}
+	}
+}
+
+local function get_filepath(hl_grps)
+	local cur_path = vim.fn.expand("%:p")
+	local home = vim.fs.find(".git", {
+		path = cur_path,
+		upward = true,
+		type = "directory"
+	})[1]
+
+	if home == nil then
+		return cur_path
+	end
+
+	home = vim.fs.dirname(home)
+
+	local root = table.concat({
+		hl_grps["Muted"],
+		vim.fs.dirname(home),
+		'/',
+		"%*"
+	})
+
+	local trunk = table.concat({
+		hl_grps["Warn"],
+		vim.fs.basename(home),
+		'/',
+		"%*",
+		vim.fn.expand("%r"),
+	})
+
+	return root .. trunk
+end
 
 
 -- Mason
